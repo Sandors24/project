20142ad3ad25278c3246faaf4c29114ccd8169be
Sandor Sandor <sandors0000@gmail.com>
2022-09-18 21:22:11 +0300

    four commit

--- project/src/components/Dialogs/Dialogs.jsx
+++ project/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,31 @@ import s from "./Dialogs.module.css"
 
 export const Dialogs = (props) => {
   return (
-    <div >
-      <h1>dilo</h1>
+    <div className={s.dialogs}><div className={s.mesagges}>
+      <div className={s.mesaje}>hi</div>
+      <div className={s.mesaje}>how are you</div>
+      <div className={s.mesaje}>you</div>
+
+    </div> 
+
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>
+      sandor
+        </div>
+
+        <div className={s.dialog}>
+      yula
+        </div>
+        <div className={s.dialog}>
+      ender
+        </div>
+        <div className={s.dialog}>
+      motor
+        </div>
+        <div className={s.dialog}>
+      hela
+        </div>
+      </div>
 
 
     </div>
--- project/src/components/Navbar/Navbar.jsx
+++ project/src/components/Navbar/Navbar.jsx
@@ -3,14 +3,15 @@ import s from "./Navbar.module.css"
 import {  NavLink } from "react-router-dom"
 
 
+
 export const Navbar = () => {
   return (
     <nav className={s.nav}>
-        <div >
+        <div className={s.item}>
             <NavLink to ='/Profile'>Profile</NavLink>
         </div>
-        <div  >
-            <NavLink to ='/Mesagges'>Messages</NavLink>
+        <div className={`${s.item} ${s.active}`} >
+            <NavLink to ='/Dialogs' >Dialogs</NavLink>
         </div>
         <div >
             <a  className={s.item} href="News">News</a>
